7d1390656f0160ff017a2c81218b7cc8fcbc151b
Mohitraikwar121 <raikwarmohit299@gmail.com>
2025-06-24 09:41:21 +0530

    Contracts/Cross-Chain NFT Teleportation.sol

--- Contracts/Cross-Chain NFT Teleportation.sol
+++ Contracts/Cross-Chain NFT Teleportation.sol
@@ -5,7 +5,6 @@ interface IERC721 {
     function ownerOf(uint256 tokenId) external view returns (address);
     function transferFrom(address from, address to, uint256 tokenId) external;
 }
-
 contract CrossChainNFTBridge {
 
     address public admin;
